468ba77e22943f95bb88be3c35a3a3e36299ade5
Aydin Taskin <DefiniDev@gmail.com>
2020-06-06 16:48:25 +0100

    feature(working_cancel_button)
    Fixed method of exit from game-loop by enclosing game-logic in function. Previously just returned a "miss" alert and re-looped. "null" value returned from player input now exit's function and skips post-game score display.

--- battleship.js
+++ battleship.js
@@ -8,40 +8,49 @@ var hit2 = false;
 var hit3 = false;
 var guesses = 0;
 var isSunk = false;
-while (isSunk == false) {
-  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-  if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number!");
-  } else {
-    guesses = guesses + 1;
-    if (guess == location1 && hit1 == false) {
-      alert("Hit forward!");
-      hit1 = true;
-    } else if (guess == location1 && hit1 == true) {
-      alert("Already hit!");
-    } else if (guess == location2 && hit2 == false) {
-      alert("Hit mid!");
-      hit2 = true;
-    } else if (guess == location2 && hit2 == true) {
-      alert("Already hit!");
-    } else if (guess == location3 && hit3 == false) {
-      alert("Hit aft!");
-      hit3 = true;
-    } else if (guess == location3 && hit3 == true) {
-      alert("Already hit!");
+function zerp() {
+  while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6 || guess == "") {
+      alert("Please enter a valid cell number!");
     } else {
-      alert("MISS!");
-    }
-    if (hit1 && hit2 && hit3 == true) {
-      isSunk = true;
-      alert("You sank my battleship!");
+      guesses = guesses + 1;
+      if (guess == location1 && hit1 == true) {
+        alert("Already hit!");
+      } else if (guess == location1 && hit1 == false) {
+        alert("Hit forward!");
+        hit1 = true;
+      } else if (guess == location2 && hit2 == true) {
+        alert("Already hit!");
+      } else if (guess == location2 && hit2 == false) {
+        alert("Hit mid!");
+        hit2 = true;
+      } else if (guess == location3 && hit3 == true) {
+        alert("Already hit!");
+      } else if (guess == location3 && hit3 == false) {
+        alert("Hit aft!");
+        hit3 = true;
+      } else if (guess == null) {
+        return;
+      } else {
+        alert("MISS!");
+      }
+      if (hit1 && hit2 && hit3 == true) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
     }
   }
 }
-var stats =
-  "You took " +
-  guesses +
-  " guesses to sink the battleship, " +
-  "which means your shooting accuracy was " +
-  3 / guesses;
-alert(stats);
+zerp();
+if (isSunk == true) {
+  var stats =
+    "You took " +
+    guesses +
+    " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " +
+    3 / guesses;
+  alert(stats);
+} else {
+  alert("Thanks for playing. Bye!");
+}
